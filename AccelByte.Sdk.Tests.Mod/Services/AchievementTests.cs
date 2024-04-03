db8c5ee4059deb74fad93cc084266e3ee64af69a
Rinardi Sarean <rinardi@accelbyte.net>
2024-04-03 14:13:57 +0700

    feat: add achievement async test

--- AccelByte.Sdk.Tests.Mod/Services/AchievementTests.cs
+++ AccelByte.Sdk.Tests.Mod/Services/AchievementTests.cs
@@ -1,15 +1,18 @@
-﻿// Copyright (c) 2022-2023 AccelByte Inc. All Rights Reserved.
+﻿// Copyright (c) 2022-2024 AccelByte Inc. All Rights Reserved.
 // This is licensed software from AccelByte Inc, for limitations
 // and restrictions contact your company contract manager.
 
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using NUnit.Framework;
 
 using AccelByte.Sdk.Core;
 using AccelByte.Sdk.Api;
 using AccelByte.Sdk.Core.Net.Http;
 using AccelByte.Sdk.Api.Achievement.Model;
+using AccelByte.Sdk.Tests.Mod.Repository;
+using AccelByte.Sdk.Core.Repository;
 
 namespace AccelByte.Sdk.Tests.Mod.Services
 {
@@ -119,6 +122,112 @@ namespace AccelByte.Sdk.Tests.Mod.Services
                 DisableRetry();
                 ModelsAchievementResponse? dResp = _Sdk.GetAchievementApi().Achievements.AdminGetAchievementOp
                     .Execute(achievement_code, _Sdk.Namespace);
+            });
+        }
+
+        [Test]
+        public async Task AchievementAsyncServiceTests()
+        {
+            var retryPolicy = HttpClientPolicy.Default;
+            retryPolicy.MaxRetryCount = 3;
+            retryPolicy.RetryInterval = 500;
+            retryPolicy.RetryLogicHandler = new ResponseCodeCheckLogicHandler("425");
+
+            var sdk = AccelByteSdk.Builder
+                .SetHttpClient(ReliableHttpClient.Builder
+                    .SetDefaultPolicy(retryPolicy)
+                    .Build())
+                .SetConfigRepository(IntegrationTestConfigRepository.Admin)
+                .UseDefaultTokenRepository()
+                .SetCredentialRepository(IntegrationTestCredentialRepository.Admin)
+                .EnableLog()
+                .Build();
+
+            await sdk.LoginUserAsync();
+
+            string achievement_code = "csharp-sdk-test-3";
+            string achievement_name = "CSharp Server SDK Test 3";
+
+            ModelsAchievementRequest newAchievement = new ModelsAchievementRequest()
+            {
+                AchievementCode = achievement_code,
+                DefaultLanguage = "en",
+                Name = new Dictionary<string, string>()
+                {
+                    {"en", achievement_name }
+                },
+                Description = new Dictionary<string, string>
+                {
+                    {"en", "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..." }
+                },
+                GoalValue = 1000.0,
+                StatCode = achievement_code,
+                Hidden = true,
+                Incremental = false,
+                LockedIcons = new List<ModelsIcon>()
+                {
+                    new ModelsIcon()
+                    {
+                        Slug = "shield-locked",
+                        Url = "https://cdn.demo.accelbyte.io/files/accelbyte/achievements/50000f325ef841a6972a005779e20991.png"
+                    }
+                },
+                UnlockedIcons = new List<ModelsIcon>()
+                {
+                    new ModelsIcon()
+                    {
+                        Slug = "shield-unlocked",
+                        Url = "https://cdn.demo.accelbyte.io/files/accelbyte/achievements/fe89fd07102f4057be202fbd3fdd9a21.png"
+                    }
+                },
+                Tags = new List<string>() { "sdk", "test", "csharp" }
+            };
+
+            ModelsAchievementResponse? cResp = await sdk.GetAchievementApi().Achievements.AdminCreateNewAchievementOp
+                .ExecuteAsync(newAchievement, sdk.Namespace);
+            Assert.IsNotNull(cResp);
+            Assert.AreEqual(cResp?.AchievementCode!, achievement_code);
+
+            ModelsAchievementUpdateRequest updateAchievement = new ModelsAchievementUpdateRequest()
+            {
+                GoalValue = 2000.0,
+                DefaultLanguage = "en",
+                Name = new Dictionary<string, string>()
+                {
+                    {"en", achievement_name }
+                },
+                Description = new Dictionary<string, string>
+                {
+                    {"en", "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..." }
+                }
+            };
+
+            ModelsAchievementResponse? uResp = await sdk.GetAchievementApi().Achievements.AdminUpdateAchievementOp
+                .ExecuteAsync(updateAchievement, achievement_code, sdk.Namespace);
+            Assert.IsNotNull(uResp);
+            Assert.AreEqual(uResp?.GoalValue!, 2000.0);
+
+            ModelsAchievementResponse? rResp = await sdk.GetAchievementApi().Achievements.AdminGetAchievementOp
+                .ExecuteAsync(achievement_code, sdk.Namespace);
+            Assert.IsNotNull(rResp);
+            Assert.AreEqual(rResp?.GoalValue!, 2000.0);
+            Assert.AreEqual(rResp?.Name!["en"], achievement_name);
+
+            ModelsPaginatedAchievementResponse? gaResp = await sdk.GetAchievementApi().Achievements.AdminListAchievementsOp
+                .SetLimit(100)
+                .SetOffset(0)
+                .ExecuteAsync(sdk.Namespace);
+            Assert.IsNotNull(gaResp);
+            Assert.Greater(gaResp?.Data?.Count, 0);
+
+            await sdk.GetAchievementApi().Achievements.AdminDeleteAchievementOp
+                .ExecuteAsync(achievement_code, sdk.Namespace);
+
+            HttpResponseException? hrx = Assert.Throws<HttpResponseException>(() =>
+            {
+                retryPolicy.RetryOnException = false;
+                sdk.GetAchievementApi().Achievements.AdminGetAchievementOp
+                    .Execute(achievement_code, sdk.Namespace);
             });
         }
     }
