ded3ecb04b721e27ea2db3a835c1f56089f7cbb8
Rinardi Sarean <rinardi@accelbyte.net>
2024-11-06 10:28:23 +0700

    feat(core): add base api response and api error class

---
+++ AccelByte.Sdk.Core/Classes/ApiError.cs
@@ -0,0 +1,85 @@
+﻿// Copyright (c) 2024 AccelByte Inc. All Rights Reserved.
+// This is licensed software from AccelByte Inc, for limitations
+// and restrictions contact your company contract manager.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccelByte.Sdk.Core
+{
+    public sealed class ApiError
+    {
+        public static readonly ApiError Empty = new ApiError("", "");
+
+        public string Code { get; }
+
+        public string Message { get; }
+
+        public bool IsAvailable { get; }
+
+        public ApiError(string code,string message)
+        {
+            Code = code;
+            Message = message;
+            IsAvailable = Code != "";
+        }
+
+        public override int GetHashCode()
+        {
+            return Code.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is ApiError)
+                return Code.Equals(((ApiError)obj).Code);
+            else
+                return false;
+        }
+
+        public Exception ThrowException()
+        {
+            return new Exception($"Code: {Code}, Message: {Message}");
+        }
+
+        #region Operator Overloading
+        public static bool operator ==(ApiError left, ApiError right)
+        {
+            if (left is null)
+                return (right is null);
+            return left.Code == right.Code;
+        }
+
+        public static bool operator !=(ApiError left, ApiError right)
+        {
+            return left.Code != right.Code;
+        }
+
+        public static bool operator ==(ApiError left, string right)
+        {
+            return left.Code == right;
+        }
+
+        public static bool operator !=(ApiError left, string right)
+        {
+            return left.Code != right;
+        }
+
+        public static bool operator ==(string left, ApiError right)
+        {
+            return left == right.Code;
+        }
+
+        public static bool operator !=(string left, ApiError right)
+        {
+            return left != right.Code;
+        }
+        #endregion
+    }
+}
---
+++ AccelByte.Sdk.Core/Classes/ApiResponse.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) 2024 AccelByte Inc. All Rights Reserved.
+// This is licensed software from AccelByte Inc, for limitations
+// and restrictions contact your company contract manager.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccelByte.Sdk.Core
+{
+    public abstract class ApiResponse
+    {
+        public string FullOperationId { get => GetFullOperationId(); }
+
+        public bool IsSuccess { get; set; } = false;
+
+        public HttpStatusCode StatusCode { get; set; } = HttpStatusCode.OK;
+
+        public string ContentType { get; set; } = "";
+
+        public ApiError Error { get; set; } = ApiError.Empty;
+
+        protected abstract string GetFullOperationId();
+
+        public void ThrowExceptionIfError()
+        {
+            if (!IsSuccess && Error.IsAvailable)
+                throw new Exception($"{FullOperationId} returns error: {Error.Code}. {Error.Message}");
+            else if (!IsSuccess)
+                throw new Exception($"{FullOperationId} returns unknown error with http status code {(int)StatusCode}");
+        }
+
+        public void Ok()
+        {
+            ThrowExceptionIfError();
+        }
+    }
+
+    public abstract class ApiResponse<TData> : ApiResponse
+    {
+        public TData? Data { get; set; } = default;
+
+        public new TData Ok()
+        {
+            ThrowExceptionIfError();
+            if (Data == null)
+                throw new Exception($"{FullOperationId} returns NULL");
+
+            return Data;
+        }
+    }
+}
