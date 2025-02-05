9cdbe4ba7424d4bca3fa11aaabd575a9ec503e17
Rinardi Sarean <rinardi@accelbyte.net>
2025-02-05 07:51:20 +0700

    feat(tests): add login queue integration tests

---
+++ AccelByte.Sdk.Tests.Mod/Services/LoginQueueTests.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) 2025 AccelByte Inc. All Rights Reserved.
+// This is licensed software from AccelByte Inc, for limitations
+// and restrictions contact your company contract manager.
+
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+using AccelByte.Sdk.Core;
+using AccelByte.Sdk.Api;
+
+using AccelByte.Sdk.Api.Loginqueue.Model;
+
+namespace AccelByte.Sdk.Tests.Mod.Services
+{
+    [TestFixture(Category = "ServiceIntegration")]
+    [Explicit]
+    public class LoginQueueTests : BaseServiceTests
+    {
+        public LoginQueueTests() : base(false) { }
+
+        [Test]
+        public void ConfigurationTests()
+        {
+            Assert.IsNotNull(_Sdk);
+            if (_Sdk == null)
+                return;
+
+            int updateMaxLoginRate = 100;
+
+            #region Get configuration
+            var config = _Sdk.GetLoginqueueApi().AdminV1.AdminGetConfigurationOp
+                .Execute(_Sdk.Namespace)
+                .EnsureSuccess();
+            #endregion
+            Assert.IsNotNull(config);
+
+            #region Update configuration
+            var configUpdate = _Sdk.GetLoginqueueApi().AdminV1.AdminUpdateConfigurationOp
+                .Execute(new ApimodelsConfigurationRequest()
+                {
+                    MaxLoginRate = updateMaxLoginRate
+                }, _Sdk.Namespace)
+                .EnsureSuccess();
+            #endregion
+            Assert.IsNotNull(configUpdate);
+            if (configUpdate != null)
+                Assert.AreEqual(updateMaxLoginRate, configUpdate.MaxLoginRate);
+        }
+    }
+}
