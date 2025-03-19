87d6821b4b15c58bfa44f683833c482461b925c7
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2025-03-19 10:45:30 +0700

    test(integration): gametelemetry - remove deprecated endpoints

--- AccelByte.Sdk.Tests.Mod/Services/GameTelemetryTests.cs
+++ AccelByte.Sdk.Tests.Mod/Services/GameTelemetryTests.cs
@@ -2,13 +2,10 @@
 // This is licensed software from AccelByte Inc, for limitations
 // and restrictions contact your company contract manager.
 
-using System;
 using System.Collections.Generic;
 using NUnit.Framework;
 
-using AccelByte.Sdk.Core;
 using AccelByte.Sdk.Api;
-using AccelByte.Sdk.Core.Net.Http;
 
 using AccelByte.Sdk.Api.Gametelemetry.Model;
 
@@ -33,9 +30,6 @@ namespace AccelByte.Sdk.Tests.Mod.Services
                 return;
             }
 
-            string steamId = "76561199259217491";
-            string playTime = "4";
-
             #region Save game telemetry event
             _Sdk.GetGametelemetryApi().GametelemetryOperations.ProtectedSaveEventsGameTelemetryV1ProtectedEventsPostOp
                 .Execute(new List<TelemetryBody>
@@ -52,31 +46,6 @@ namespace AccelByte.Sdk.Tests.Mod.Services
                 })
                 .EnsureSuccess();
             #endregion
-
-            DisableRetry();
-            #region Update steam's playtime
-            try
-            {
-                _Sdk.GetGametelemetryApi().GametelemetryOperations.ProtectedUpdatePlaytimeGameTelemetryV1ProtectedSteamIdsSteamIdPlaytimePlaytimePutOp
-                    .Execute(playTime, steamId)
-                    .EnsureSuccess();
-            }
-            catch (Exception e)
-            {
-                if (e.Message.ToLower().Contains("user not found"))
-                {
-                    Assert.Ignore("User not found.");
-                }
-            }
-            #endregion
-
-            #region Get steam's playtime
-            PlayTimeResponse resGet = _Sdk.GetGametelemetryApi().GametelemetryOperations
-                .ProtectedGetPlaytimeGameTelemetryV1ProtectedSteamIdsSteamIdPlaytimeGetOp
-                .Execute(steamId)
-                .EnsureSuccess();
-            #endregion
-            Assert.Equals(playTime, resGet.TotalPlaytime!);
         }
     }
 }
