fab80e0d38cfdecd5a2c013f07bfd4fb46d08362
Rinardi Sarean <rinardi@accelbyte.net>
2026-02-23 13:53:32 +0700

    feat: update compatibility lib and regenerate compatible op builder

--- AccelByte.Sdk.Compat/Core/AccelByteSDK.cs
+++ AccelByte.Sdk.Compat/Core/AccelByteSDK.cs
@@ -5,58 +5,20 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-using AccelByte.Sdk.Api;
-using AccelByte.Sdk.Api.Achievement;
-using AccelByte.Sdk.Api.Ams;
-using AccelByte.Sdk.Api.Basic;
-using AccelByte.Sdk.Api.Chat;
-using AccelByte.Sdk.Api.Cloudsave;
-using AccelByte.Sdk.Api.Gametelemetry;
-using AccelByte.Sdk.Api.Gdpr;
-using AccelByte.Sdk.Api.Group;
-using AccelByte.Sdk.Api.Iam;
-using AccelByte.Sdk.Api.Inventory;
-using AccelByte.Sdk.Api.Leaderboard;
-using AccelByte.Sdk.Api.Legal;
-using AccelByte.Sdk.Api.Lobby;
-using AccelByte.Sdk.Api.Match2;
-using AccelByte.Sdk.Api.Platform;
-using AccelByte.Sdk.Api.Reporting;
-using AccelByte.Sdk.Api.Seasonpass;
-using AccelByte.Sdk.Api.Session;
-using AccelByte.Sdk.Api.Social;
-using AccelByte.Sdk.Api.Ugc;
 using AccelByte.Sdk.Authentication;
 using AccelByte.Sdk.Core.Repository;
+using AccelByte.Sdk.Core.Security;
+using AccelByte.Sdk.Feature.AutoTokenRefresh;
 
 namespace AccelByte.Sdk.Core
 {
-    public class AccelByteSDK : AccelByteSdk
+    public partial class AccelByteSDK : AccelByteSdk
     {
-        public static new IAccelByteSdkBuilder<AccelByteSDK> Builder { get => new AccelByteSdkBuilder<AccelByteSDK>(); }
-
-        #region Api Fluent Properties
-        public AchievementApi Achievement { get => ((IAccelByteSdk)this).GetAchievementApi(); }
-        public AmsApi Ams { get => ((IAccelByteSdk)this).GetAmsApi(); }
-        public BasicApi Basic { get => ((IAccelByteSdk)this).GetBasicApi(); }
-        public ChatApi Chat { get => ((IAccelByteSdk)this).GetChatApi(); }
-        public CloudsaveApi Cloudsave { get => ((IAccelByteSdk)this).GetCloudsaveApi(); }
-        public GametelemetryApi Gametelemetry { get => ((IAccelByteSdk)this).GetGametelemetryApi(); }
-        public GdprApi Gdpr { get => ((IAccelByteSdk)this).GetGdprApi(); }
-        public GroupApi Group { get => ((IAccelByteSdk)this).GetGroupApi(); }
-        public IamApi Iam { get => ((IAccelByteSdk)this).GetIamApi(); }
-        public InventoryApi Inventory { get => ((IAccelByteSdk)this).GetInventoryApi(); }
-        public LeaderboardApi Leaderboard { get => ((IAccelByteSdk)this).GetLeaderboardApi(); }
-        public LegalApi Legal { get => ((IAccelByteSdk)this).GetLegalApi(); }
-        public LobbyApi Lobby { get => ((IAccelByteSdk)this).GetLobbyApi(); }
-        public Match2Api Match2 { get => ((IAccelByteSdk)this).GetMatch2Api(); }
-        public PlatformApi Platform { get => ((IAccelByteSdk)this).GetPlatformApi(); }
-        public ReportingApi Reporting { get => ((IAccelByteSdk)this).GetReportingApi(); }
-        public SeasonpassApi Seasonpass { get => ((IAccelByteSdk)this).GetSeasonpassApi(); }
-        public SessionApi Session { get => ((IAccelByteSdk)this).GetSessionApi(); }
-        public SocialApi Social { get => ((IAccelByteSdk)this).GetSocialApi(); }
-        public UgcApi Ugc { get => ((IAccelByteSdk)this).GetUgcApi(); }
-        #endregion
+        public static new IAccelByteSdkBuilder<AccelByteSDK> Builder
+        {
+            //Monolithic version use default token validator by default.
+            get => (new AccelByteSdkBuilder<AccelByteSDK>()).UseDefaultTokenValidator();
+        }
 
         public AccelByteSDK(IAccelByteConfig config)
             : base(config)
@@ -81,6 +43,10 @@ namespace AccelByte.Sdk.Core
         {
 
         }
+
+        #region Authentication
+
+        #endregion
     }
 
     public static class AccelByteSDKBuilder_Exts
@@ -113,6 +79,14 @@ namespace AccelByte.Sdk.Core
         public static IAccelByteSdkBuilder<AccelByteSDK> UseDefaultTokenValidator(this IAccelByteSdkBuilder<AccelByteSDK> builder)
         {
             return builder.SetTokenValidator(new DefaultTokenValidator());
+        }
+
+        public static IAccelByteSdkBuilder<AccelByteSDK> UseRefreshIfPossible(this IAccelByteSdkBuilder<AccelByteSDK> builder)
+            => builder;
+
+        public static IAccelByteSdkBuilder<AccelByteSDK> SetOnAfterLoginAction(this IAccelByteSdkBuilder<AccelByteSDK> builder, Action<LoginType, AuthActionType, ITokenResponse?, IAccelByteSdk> action)
+        {
+            return builder.RegisterEvent(Auth_SdkExtensions.ON_AFTER_LOGIN_EVENT_KEY, action);
         }
     }
 }
